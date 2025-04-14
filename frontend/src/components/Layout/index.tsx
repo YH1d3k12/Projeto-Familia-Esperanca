fbc70cc8b37e76c498c86112775ce2c2ef8eae10
Djalma <djalmahy@hotmail.com>
2025-04-14 18:01:47 -0300

    Feat: created navbar.

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
-// import Navbar from '../Navbar';
+import Navbar from '../Navbar';
 import './styles.css';
 
 export default function Layout() {
     return (
         <div className="layout-container">
-            {/* <Navbar /> */}
+            <Navbar />
             <Outlet />
         </div>
     );
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,54 @@
+import { Link } from 'react-router-dom';
+import './styles.css';
+import { useState } from 'react';
+
+export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleToggle = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <header className="navbar-container">
+            <nav className="navbar">
+                <div>Icone vai aqui</div>
+                <div>
+                    <input
+                        type="checkbox"
+                        id="menuToggle"
+                        onClick={handleToggle}
+                    />
+                    <label htmlFor="menuToggle" className="navbar-burguer ">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </label>
+                </div>
+                <ul className={`navbar-options ${isOpen ? 'active' : ''}`}>
+                    <li>
+                        <Link to={'/home'}>Início</Link>
+                    </li>
+                    <li>
+                        <Link to={'/about'}>Quem somos</Link>
+                    </li>
+                    <li>
+                        <Link to={'/join'}>Faça parte</Link>
+                    </li>
+                    <li>
+                        <Link to={'/donation'}>Contribua</Link>
+                    </li>
+                    <li>
+                        <Link to={'/ministry'}>Ministérios</Link>
+                    </li>
+                    <li>
+                        <Link to={'/cell'}>Nossas Células</Link>
+                    </li>
+                    <li>
+                        <Link to={'/events'}>Eventos e Cursos</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
