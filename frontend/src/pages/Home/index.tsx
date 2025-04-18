f10270654176aee37b5cd9b6d1d61732fda66b04
Djalma <djalmahy@hotmail.com>
2025-04-17 21:12:35 -0300

    Feat: created hero page.

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,15 +1,14 @@
+import video from '../../assets/video.mp4';
 import './styles.css';
 
 function Home() {
     return (
-        <main className="page-screen home">
-            <section className="home-hero">
-                <div className="home-hero-image">
-                    <img
-                        src="https://images.unsplash.com/photo-1662826312329-00bca2d79204?q=80&w=2104&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                        alt=""
-                    />
-                </div>
+        <main className="multi-page-container home">
+            <section className="home-hero page-screen">
+                <video className="hero-video" autoPlay loop muted playsInline>
+                    <source src={video} type="video/mp4" />
+                </video>
+
                 <div className="home-hero-text">
                     <h1>
                         Uma família para
